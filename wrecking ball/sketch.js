a513cb7761c3e0e0b602f1857e63d46d31fe30a0
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-08-25 14:28:22 +0530

    Update sketch.js

--- wrecking ball/sketch.js
+++ wrecking ball/sketch.js
@@ -5,7 +5,7 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10,box11,box12,box13,box14,box15,box16,box17,box18,box19,box20, ground;
-var ball,ball1,ball2,ball3,ball4, sling,sling1,sling2,sling3,sling4 
+var ball, rope
 
 function setup() {
   createCanvas(3000, 800);
@@ -44,47 +44,33 @@ function draw() {
   background(180);
   Engine.update(engine);
   ground.display();
-  // box1.display();
-  // box2.display();
-  // box3.display();
-  // box4.display()
-  // box5.display()
-  // box6.display()
-  // box7.display()
-  // box8.display()
-  // box9.display()
-  // box10.display()
-  // box11.display()
-  // box12.display()
-  // box13.display()
-  // box14.display()
-  // box15.display()
-  // box16.display()
-  // box17.display()
-  // box18.display()
-  // box19.display()
-  // box20.display()
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display()
+  box5.display()
+  box6.display()
+  box7.display()
+  box8.display()
+  box9.display()
+  box10.display()
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box16.display()
+  box17.display()
+  box18.display()
+  box19.display()
+  box20.display()
   
 
   ball.display();
   rope.display();
 
 
-  ball1.display();
-  sling1.display();
 
-
-  ball2.display();
-  sling2.display();
-
-
-  ball3.display();
-  sling3.display();
-
-
-  ball4.display();
-  sling4.display();
-  // mouse  Pressed();
 }
 
 

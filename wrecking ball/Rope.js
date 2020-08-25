a6cd26449125276af6157e12c446db1040e70c13
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-08-25 14:27:31 +0530

    Update Rope.js

--- wrecking ball/Rope.js
+++ wrecking ball/Rope.js
@@ -1,4 +1,4 @@
-class Sling {
+class Rope {
   constructor(bodyA, pointB) {
     var options = {
       bodyA: bodyA,
@@ -8,20 +8,20 @@ class Sling {
     };
 
     this.pointB = pointB;
-    this.sling = Constraint.create(options);
-    World.add(world, this.sling);
+    this.rope = Constraint.create(options);
+    World.add(world, this.rope);
   }
   attach(body) {
-    this.sling.bodyA = body;
+    this.rope.bodyA = body;
   }
 
   fly() {
-    this.sling.bodyA = null;
+    this.rope.bodyA = null;
   }
 
   display() {
-    if (this.sling.bodyA) {
-      var pointA = this.sling.bodyA.position;
+    if (this.rope.bodyA) {
+      var pointA = this.rope.bodyA.position;
       var pointB = this.pointB;
       push();
 
